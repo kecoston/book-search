818671a1b9fe3d790bc9c40053800f19d3778771
Kathryn Sherazi <67169418+kecoston@users.noreply.github.com>
2020-12-08 12:23:19 -0600

    added header component

---
+++ client/src/components/header/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header () {
+    return (
+        <div className="Jumbotron">
+            <h1>React Google Book Search</h1>
+            <h3>Search for and Save Books of Interest</h3>
+            <img src="/public/images/book-search-image.jpg" alt="Book-Search" />
+        </div>
+    )
+}
+
+export default Header 
