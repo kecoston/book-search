671d9055de1430b7f5e12a75097dee35e9addde2
Kathryn Sherazi <67169418+kecoston@users.noreply.github.com>
2020-12-08 14:34:41 -0600

    search component added

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,37 @@
 import React, { Component } from "react";
 import "./App.css";
-import Header from "./components/header/header"
+import Header from "./components/Header/header"
+import Search from "./components/Search/search"
 
-function App() {
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchValue: "",
+      searchInput: ""
+    };
+  }
+
+  searchInput = (search) => {
+    this.setState({searchValue: search})
+  }
+
+  render() {
+
+    console.log(this.state.searchValue)
+
   return (
     <div>
  
     <Header/>
+    <Search  
+        search={this.searchInput}
+    />
     
     </div>
   );
+}
 }
 
 
---
+++ client/src/components/Search/search.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./search.css"
+
+function Search(props) {
+    return (
+        <div className="container">
+
+            <div className="form-group">
+                <label for="searchInput">Search for Books</label>
+                <input 
+                    type="search" class="form-control" id="searchInput"></input>
+
+                    <button type="submit" className="btn btn-primary" onClick={ () => props.search(document.getElementById("searchInput").value)}>Submit</button>
+            </div>
+
+        </div>
+
+  )
+}
+
+export default Search
