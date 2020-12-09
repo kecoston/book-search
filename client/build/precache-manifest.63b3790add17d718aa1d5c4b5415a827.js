4971aebb14993519369fb6c7eb5efff46a92caf7
Kathryn Sherazi <67169418+kecoston@users.noreply.github.com>
2020-12-09 16:40:15 -0600

    deploying

---
+++ client/build/precache-manifest.63b3790add17d718aa1d5c4b5415a827.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13c88d0d52100291748c151f3f2311e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f77bd9f2e9ec3a3a4485",
+    "url": "/static/css/2.99a2b36c.chunk.css"
+  },
+  {
+    "revision": "78e6afb53ed157926e1f",
+    "url": "/static/css/main.aed265a2.chunk.css"
+  },
+  {
+    "revision": "f77bd9f2e9ec3a3a4485",
+    "url": "/static/js/2.20e48b7e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.20e48b7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78e6afb53ed157926e1f",
+    "url": "/static/js/main.456e6bab.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
